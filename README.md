b18bbad15b598b0da361143d805922c7d6b79cee
Adam Vukoje <adam.vukoje@gmail.com>
2023-10-12 23:33:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A projcet to pracitce Selenium.
+The idea is to use Selenium to automate airplane ticket reservation.
+The user can choose what's the criteria when there is more than one ticket available:
+- Ticket price
+- Overall travel time
+- Time of departure
+- Time of arrival
---
+++ src/main/java/org/MyCompany/Main.java
@@ -0,0 +1,15 @@
+package org.MyCompany;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://keyflight.io/fake");
+        driver.quit();
+    }
+}
